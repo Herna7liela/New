19dc26c65d731ba8ac6c0ccb9ad404d56b961a2b
Herna7liela <herna.dewit@gmail.com>
2015-03-20 11:50:51 +0200

    changes

---
+++ while15.py
@@ -0,0 +1,23 @@
+# Write a program that asks the user for a sequence of floats, ending in a blank line. The user is then allowed to give one of the following commands:
+#   mean: prints the mean of the numbers
+#   sum: prints the sum of the numbers
+#   quit: exits the program
+#If 'mean' or 'sum' are entered, the program responds and then waits for more input.
+
+numfloats = input("Enter a float: ")
+total = 0
+count = 0
+while numfloats != "":
+    if numfloats != "":
+        numfloats = float(numfloats)
+        total = total + numfloats
+        count = count + 1    
+    elif numfloats == "":
+        print ("Exiting program")
+    numfloats = input("Enter a float: ")        
+
+nummean = (total/count)
+numsum = total        
+print (nummean)
+print (numsum)  
+        
