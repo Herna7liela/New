5eaadc2ed3a10a8ec4ca68d099ce4d5e10263617
Herna7liela <herna.dewit@gmail.com>
2015-03-20 14:06:11 +0200

    changes

--- while15.py
+++ while15.py
@@ -1,4 +1,5 @@
-# Write a program that asks the user for a sequence of floats, ending in a blank line. The user is then allowed to give one of the following commands:
+# Write a program that asks the user for a sequence of floats, ending in a blank line.
+# The user is then allowed to give one of the following commands:
 #   mean: prints the mean of the numbers
 #   sum: prints the sum of the numbers
 #   quit: exits the program
@@ -11,13 +12,23 @@ while numfloats != "":
     if numfloats != "":
         numfloats = float(numfloats)
         total = total + numfloats
-        count = count + 1    
-    elif numfloats == "":
-        print ("Exiting program")
-    numfloats = input("Enter a float: ")        
-
+        count = count + 1  
+    numfloats = input("Enter a float: ") 
+    
 nummean = (total/count)
 numsum = total        
 print ("The mean of the entered floats are ", nummean)
-print ("The sum of the entered floats are ", numsum)  
+print ("The sum of the entered floats are ", numsum)      
+      
+for count in range(0:):           
+    numfloats = input("Enter a float: ")
+# use same idea as with average2.py, but 
+# figure out a way to keep on repeating the things
+#while numfloats != "":
+#numfloats = input("Enter a float: ")
         
+# so you have to be able to ask to print something and then get the value of it. 
+# use a while loop to make it go on and on
+# must put in something so that you can then ask the program to quite after which it
+# will leave the while loop
+# the while loop can only be exited when the condition is not met anymore
