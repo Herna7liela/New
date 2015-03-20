67f5107055c81838ff724f1c42a1e0be698b67e2
Herna7liela <herna.dewit@gmail.com>
2015-03-20 10:20:28 +0200

    changes

--- names2.py
+++ names2.py
@@ -5,22 +5,22 @@
 
 name = input("Type in a name: ")
 othername = "ZZZZZ"                               
-
+length = "aaaaaaaaaaaa"
 while name != "":
-        if "name" < "othername":
-            aname = name
-        if "othername" > "name":    
-            bname = name
+        if name > othername:
+                aname = name
+        if name < aname:    
+                bname = name
+        if len(name) < len(length):
+                shortname = name
+        elif len(name) > len(shortname):
+                longname = name
+        name = input("Type in a name: ")   # when asking for this name it also has to be inside the loop in order to not just stay in the loop without any repeats
             
-print (aname)
-print (bname)
-        ##else:
-            #shortest = 
-##else:
-            #longest = can use the length len() to determine this
-        
-    
-#print (sort(name))               
-#print ("The shortest name is ", shortest) 
-#print ("The longest name is ", longest)
+print ("First alphabetical name ", aname)
+print ("Last aplhabetical name ", bname)
+print ("The shortest name is ", shortname)
+print ("The longest name is ", longname)
+       
+ 
     
