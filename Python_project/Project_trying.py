9516293b8c240fd1f9b28131224c32e5a3701ab8
Herna7liela <herna.dewit@gmail.com>
2015-03-31 10:37:32 +0200

    new folder

---
+++ Python_project/Project_trying.py
@@ -0,0 +1,15 @@
+# STEP 1:
+# have to start by opening a a reader. The file to open is the argument to the python program
+# python3 Project_final_code.py file.gbk
+
+# STEP 2:
+# set up a structure to be able to answer questions
+# try to possible get the script of how to parse files with biopython!!!!
+# look at bioparsers and biogenbank
+
+
+
+# create a regular expression
+# use the pipe 
+# import module from builtin re and then search for expression
+# use re.compile to create the expression that will search for the expression
