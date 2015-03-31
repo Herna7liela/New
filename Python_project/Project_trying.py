89220f4641d153f10d893a223eaa09e584c531c0
Herna7liela <herna.dewit@gmail.com>
2015-03-31 12:04:58 +0200

    planning

--- Python_project/Project_trying.py
+++ Python_project/Project_trying.py
@@ -1,13 +1,17 @@
 # STEP 1:
+# get the file in the correct format .pdk to be able to work with it
 # have to start by opening a a reader. The file to open is the argument to the python program
 # python3 Project_final_code.py file.gbk
 
 # STEP 2:
+# get the .pdk file content into exp text = "...."
 # set up a structure to be able to answer questions
-# try to possible get the script of how to parse files with biopython!!!!
-# look at bioparsers and biogenbank
+## try to possible get the script of how to parse files with biopython!!!!
+## look at bioparsers and biogenbank
+# 
 
-
+# STEP 3:
+# 
 
 # create a regular expression
 # use the pipe 
